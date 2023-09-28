b3a9df8c1d2d564dc8120cfa4b0e689dd52f3fc9
Soham Gon <70486873+SOH69@users.noreply.github.com>
2023-09-28 23:25:41 +0530

    fix to error dc list if empty

--- ui/src/components/playerlist.svelte
+++ ui/src/components/playerlist.svelte
@@ -3,7 +3,7 @@
     import { PLAYERS } from "@store/stores"
 </script>
 <div class = "w-full max-h-[70.7vh] overflow-scroll no-scrollbar flex flex-col gap-[2px] items-center pb-1 pt-3">
-    {#if $PLAYERS != undefined}
+    {#if $PLAYERS != undefined && $PLAYERS.players.length != 0} 
         {#each $PLAYERS.players as player}
             <Players player={player}/>
         {/each}
